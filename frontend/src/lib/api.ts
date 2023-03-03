791abd0a492903aff0fafadfaaca71176bdaf7b5
William Chen <github@iamchen.com>
2023-03-03 13:01:17 -0500

    Add env switching

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { config } from 'util/env'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: config.baseUrl,
   headers: { 'Access-Control-Allow-Origin': '*' },
 })
 
---
+++ frontend/src/util/env.ts
@@ -0,0 +1,15 @@
+
+const nodeEnv = process.env.NODE_ENV
+export const isDevEnv = !nodeEnv
+
+console.log(`NODE_ENV is ${nodeEnv}`)
+
+export interface Config {
+  baseUrl: string
+}
+
+export const config: Config = isDevEnv ? {
+  baseUrl: 'http://localhost:8080',
+} : {
+  baseUrl: 'https://wwchen-posthog-backend.herokuapp.com',
+}
